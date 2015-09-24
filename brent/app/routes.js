e0d8f6b024d4ec5178bdcd30c6c9c9bc39aa83f7
vietpn <vietpnk53@gmail.com>
2015-09-24 14:00:50 +0700

    commit code

--- brent/app/routes.js
+++ brent/app/routes.js
@@ -14,18 +14,32 @@ module.exports = function(app){
         })
     })
 
-    app.post('signup', function(req, res){
-        var newUser = new User()
+    app.post('/signup', function(req, res){
+        var newUser = new User();
 
-        newUser.local.username = req.body.username
-        newUser.local.password = req.body.password
+        newUser.local.username = req.body.username;
+        newUser.local.password = req.body.password;
+
+        console.log("post recevied: %s %s", req.body.email, req.body.password);
 
         // save database and throw error
         newUser.save(function(err){
-            if(err) throw err
+            if(err) throw err;
         })
 
         // redirect to index
         res.redirect('/')
+    })
+
+    app.get('/:username/:password', function(req, res){
+        var newUser = new User();
+        newUser.local.username = req.params.username;
+        newUser.local.password = req.params.password;
+        console.log(newUser.local.username + " " + newUser.local.password);
+        newUser.save(function(err){
+            if(err)
+                throw err;
+        });
+        res.send("Success!");
     })
 }
