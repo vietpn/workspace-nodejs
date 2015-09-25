27c8201d94c1eb7769aa6af784266d06e2092f1e
vietpn <vietpnk53@gmail.com>
2015-09-25 17:35:55 +0700

    update

--- brent/app/routes.js
+++ brent/app/routes.js
@@ -57,7 +57,7 @@ module.exports = function(app, passport){
     /**
      * Facebook
      */
-    app.get('/auth/facebook', passport.authenticate('facebook', {scope : ['email']}));
+    app.get('/auth/facebook', passport.authenticate('facebook', {authType: 'rerequest', scope : ['public_profile', 'email']}));
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {
         successRedirect: '/profile', failureRedirect: '/'
