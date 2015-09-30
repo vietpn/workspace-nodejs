c340b199150efa5f4e63829b0b8eb66875cc81db
vietpn <vietpnk53@gmail.com>
2015-09-30 15:18:36 +0700

    update

--- brent/app/routes/api.js
+++ brent/app/routes/api.js
@@ -7,7 +7,9 @@ var express = require('express'),
 
 module.exports = function(passport){
 
-    router.use(passport.authenticate('bearer', { session: false }))
+    // check authenticate before all actions
+    //router.use(passport.authenticate('bearer', { session: false }))
+
     router.use(function(req, res, next){
         fs.appendFile('logs.txt', req.path + " token: " + req.query.access_token + "\n",
             function(err){
@@ -15,8 +17,18 @@ module.exports = function(passport){
             });
     })
 
+    // need authenticated
+    router.get('/testAPI', function(req, res, next){
+        if(req.query.access_token) next();
+        else next('route')
+    }, passport.authenticate('bearer', {session: false}),
+        function(req, res){
+            res.json({secretData : '123', Authenticated: true});
+        })
+
+    // don't need authenticated
     router.get('/testAPI', function(req, res){
-        res.json({secretData : '123'});
+        res.json({secretData : '123', Authenticated: false});
     })
 
     return router;
